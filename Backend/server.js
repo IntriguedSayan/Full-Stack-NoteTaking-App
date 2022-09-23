1d7fbbf2dc6f5346ebe9284b22cb7ae5cc0c56ba
IntriguedSayan <sayan.mid590@gmail.com>
2022-09-23 10:10:03 +0530

    frontend started

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,14 @@ const express=require("express")
 const connection=require("./Config/db")
 const {authController}=require("./Routes/auth.Route")
 const {notesController}=require("./Routes/notes.Route")
+const cors=require("cors")
 
 const app=express()
 
 const PORT=7600
 
 app.use(express.json())
+app.use(cors())
 app.use("/auth",authController)
 app.use("/notes",notesController)
 
